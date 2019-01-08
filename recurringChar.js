336c6c6cdf5f65b78e48733622a2eaba3473774a
Mona Bocharova <bocharova.mona@gmail.com>
2019-01-07 18:39:35 -0800

    solved recurring char problem using bit manipulation

---
+++ recurringChar.js
@@ -0,0 +1,13 @@
+const recurringChar = (str) => {
+	let checker = 0;
+	for(let i = 0; i < str.length; i++) {
+		let val = str.charCodeAt(i)- 65; 
+		if((checker & (1 << val)) > 0) {
+			return str[i];
+		} 
+		checker |= (1 << val);	
+	}
+	return null;
+}
+
+console.log(recurringChar('ABCBC'));
